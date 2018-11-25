4b1942c1967da3f32a3ea1a739de49085c2a1c44
Timothy H. Savage <ts2468@nyu.edu>
2018-11-25 14:44:50 -0500

    Add files via upload

--- Session 1.R
+++ Session 1.R
@@ -5,6 +5,10 @@
 library(PASWR2)
 library(MASS)
 library(repmis)
+library(latex2exp)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
 
 getwd() # Identify the working directory.
 
@@ -47,9 +51,38 @@ fredr_set_key('30e6ecb242a73869e11cb35f6aa3afc3') # My key, please don't abuse.
 gdp_pc = fredr('A229RX0A048NBEA') # Grab GDP per capita
 attach(gdp_pc) # Attach and plot.
 plot(date, value)
+
 plot(date, value, col = 'red', pch=16)
+
 plot(date, value, col = 'blue', pch=16)
+
 detach(gdp_pc)
+
+
+unrate <- fredr(series_id = "UNRATE", observation_start = as.Date("1990-01-01"))
+plot(unrate$date, unrate$value, col = 'blue', pch=16, ylab = "Rate", xlab = "Date")
+
+
+tenyear <- fredr(series_id = "DGS10", observation_start = as.Date("1990-01-01"))
+plot(tenyear$date, tenyear$value, col = 'blue', pch=16, ylab = "Rate", xlab = "Date")
+
+
+fredr_series_search_text(
+  search_text = "federal funds",
+  order_by = "popularity",
+  sort_order = "desc",
+  limit = 1) %>%
+  pull(id) %>%
+  fredr(series_id = .)
+
+
+fedfunds <- fredr(series_id = "FEDFUNDS", observation_start = as.Date("1990-01-01"))
+plot(fedfunds$date, fedfunds$value, col = 'blue', pch=16, ylab = "Rate", xlab = "Date")
+
+
+ggplot(data = fedfunds, mapping = aes(x = date, y = value, color = series_id)) +
+geom_line() + labs(x = "Date", y = "Rate", color = "Series")
+
 
 griliches = read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/Ecdat/Griliches.csv")
 attach(griliches)
@@ -64,6 +97,14 @@ attach(griliches)
 plot(age, lw80, col='blue', pch=16, xlab="Age", ylab="Log of Hourly Wages", main = "A Scatterplot")
 plot(age2, lw80, col='blue', pch=16, xlab="Age Squared", ylab="Log of Hourly Wages", main = "A Scatterplot")
 detach(griliches)
+
+
+d <- read_csv("https://stats.idre.ucla.edu/stat/data/hsbraw.csv")
+d
+mean(d$read)
+median(d$read)
+var(d$read)
+
 
 # Some very useful code for plotting the parabola: y = x ^ 2.
 par(mfrow=c(3, 3), pty = "m")  #3 by 3 layout
@@ -127,6 +168,7 @@ points(x = seq(1, 15, 2), y = rep(-1, 8), pch = 16:23, cex = 2)
 text(x = seq(1, 15, 2) + 0.7, y = rep(-1, 8), labels = paste(16:23),
      cex = 2)
 
+
 # In-class lab.  Import mtcars data and generate some plots.  Fit a linear model.
 data("mtcars")
 attach(mtcars)
@@ -150,6 +192,7 @@ plot(m, p, col = "blue", pch = 16,
 abline(h = 0.5, lty = 4, col = "red")      # Add horizontal line
 abline(v = 23, lty = 4, col = "red")       # Add vertical line
 
+
 # Conditional probability.
 library(MASS)  # used for fractions function
 Omega <- expand.grid(roll1 = 1:6, roll2 = 1:6)
@@ -168,6 +211,7 @@ PFgE <- PFaE/PE  # P(H|G)
 fractions(PFgE)
 
 
+# Monte Hall Problem
 set.seed(1984)  # Done for reproducibility
 size <- 10000000
 actual <- sample(x = 1:3, size = size, replace = TRUE)
@@ -180,6 +224,7 @@ Probs <- c(PNoSwitch, PSwitch)
 names(Probs) <- c("P(Win Given No Door Change)", "P(Win Given Door Change)")
 Probs
 
+
 set.seed(1929)
 opar <- par(no.readonly = TRUE)
 library(MASS)  # used for fractions function
@@ -189,6 +234,7 @@ n.heads <- apply(Omega, 1, sum)
 cbind(Omega, n.heads)
 table <- table(n.heads)/length(n.heads)
 fractions(table)
+
 
 plot(table, xlab = "x", ylab="P(X = x)", yaxt = "n", main = "PDF for X")
 axis(2, at = c(1/8, 3/8), labels = c("1/8", "3/8"), las = 1)
@@ -200,8 +246,10 @@ segments(1, 1/8, 1, 4/8, lty = 2)
 text(2.6, 2.5/8, "P(X = 1) =  F(1) - F(0)")
 par(opar)
 
+
 mean(n.heads)
 var(n.heads)
+
 
 library(ggplot2)
 x <- seq(-1, 1, length = 500)
@@ -235,20 +283,23 @@ plot(ecdf(xs), main = "CDF of X~Bin(8, 0.3)",
 par(opar)
 
 
-library(PASWR2)
 x <- 0:10
 n <- 10
-DF <- stack(list('$\\pi = 0.2$' = dbinom(x, n, .2), '$\\pi = 0.5$' = dbinom(x, n, .5), '$\\pi = 0.8$' = dbinom(x, n, 0.8)))
+DF <- stack(list('0.2' = dbinom(x, n, 0.2), 
+                 '0.5' = dbinom(x, n, 0.5), 
+                 '0.8' = dbinom(x, n, 0.8)))
 names(DF) <- c("px", "pi")
 DF$x <- x
 previous_theme <- theme_set(theme_bw()) # set black and white theme
+
 ggplot(data = DF, aes(x = x, y = px)) + facet_grid(pi~.) + 
   geom_linerange(aes(x = x, ymin = 0, ymax = px), size = 1) + 
-  geom_point(color = "skyblue3", size = 3) + 
+  geom_point(color = "blue", size = 3) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   theme(axis.title.y=element_text(vjust = 0.2)) +
-  labs(y = "Probability", x = "$x$", title = "$X \\sim Bin(n = 10, \\pi)$\n")
+  labs(y = "Probability", x = TeX("$x$"), title = TeX("$X \\sim Bin(n = 10, \\pi)$\n"))
 theme_set(previous_theme) # Restore original theme
+
 
 bino.gen <- function (samples = 10000, n = 20, pi = 0.5)
 {
@@ -271,6 +322,7 @@ bino.gen <- function (samples = 10000, n = 20, pi = 0.5)
 }
 
 
+# Geometric PDF and CDF
 opar <- par(no.readonly = TRUE)
 par(mfrow=c(1, 2), pty = "s")
 x <- 0:15
@@ -283,7 +335,7 @@ plot(ecdf(xs), main = "CDF of X ~ Geom(0.3)",
 par(opar)
 
 
-library(ggplot2)
+# Continuous Uniform
 x <- seq(-1, 1, length = 500)
 y <- dunif(x, -1, 1)
 DF <- data.frame(fx = y)
@@ -295,6 +347,7 @@ ggplot(data = DF, aes(x = x, y = fx)) +
   theme_set(previous_theme)                # Restore original theme
 
 
+#Continuous Gamma
 x <- seq(-1, 11, length = 500)
 y1 <- dgamma(x, 2, 1)
 y2 <- dgamma(x, 2, 2)
@@ -320,15 +373,16 @@ DF$l[3501:4000] = "lambda = 2"
 DF$l[4001:4500] = "lambda = 4"
 DF$x = x
 
+
 previous_theme <- theme_set(theme_bw()) # set black and white theme
 ggplot(data = DF, aes(x = x, y = fx)) + geom_area(fill = "blue") + facet_grid(l~a) + 
-  labs(x = "x", y = "f(x)", title = "X  ~ Gamma(alpha, lambda)") + 
+  labs(x = TeX("$x$"), y = TeX("$f(x)$"), title = TeX("$X ~ \\Gamma(\\alpha, \\lambda)$")) + 
   theme(axis.title.y=element_text(vjust = 0.2)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
 theme_set(previous_theme) # Restore original theme  
 
 
-# The Nomral Distribution
+# The Nomral Distribution: Nature's Distribution
 par(mfrow = c(1, 3))
 # First
 curve(dnorm(x, 0, 1/2), -3.3/2, 3.3/2, axes = FALSE, ann = FALSE, 
@@ -354,24 +408,28 @@ text(1, .09, "sigma")
 #
 par(mfrow=c(1,1))
 
+
 # Suppose we wanted to calcuate the probability that a value falls between 
 # A and B for a Normal(0, 1).  Let A = -1.96 and B be 1.96.
-
-curve(dnorm(x, 0, 1), xlim = c(-3, 3), ylab = "Normal(0,1)", xlab = "Range")
+curve(dnorm(x, 0, 1), xlim = c(-3, 3), ylab = "Normal(0,1)", xlab = "Range", main="We Will See This Alot")
 abline(v=-1.96, col='red', lw=2)
 abline(v=1.96, col='red', lw=2)
 pnorm(1.96, 0, 1) - pnorm(-1.96, 0, 1)
 
+
+# Exploratory Data Analysis Using Real-World Data
 data("SCORE")
 attach(SCORE)
 hist(scores, freq = F)
-eda(scores) # Incredibly powerful command in R for exploratory 
+
+eda(scores) # Incredibly powerful command in R for exploratory data analysis
 detach(SCORE)
 
 data("mtcars")
 attach(mtcars)
 eda(mpg)
 eda(hp)
+
 
 # Examine skewness in data.
 set.seed(1776)
@@ -399,8 +457,8 @@ f2 <- function(x, y){ifelse(x >= y, x*y, 0)}
 persp(x, y, outer(x, y, f2), shade = 0.6, expand = 0.6,
       theta = 300, phi = 30, ticktype = "detailed", zlab = "z")
 
+
 # Covariace and Correlation
-library(latex2exp)
 opar <- par(no.readonly = TRUE) # copy of current settings
 par(mfrow= c(1, 3), mar = c(4.1,4.1,5.1,1.1))
 x1 <- c(58, 72, 72, 86, 86, 100, 100, 114, 114, 128)
@@ -431,3 +489,173 @@ cor(x2, y2)
 cor(x3, y3)
 
 cov(x1, x1) - var(x1)
+
+
+# Bivariate Normal
+f1 <- function(x, y, p = 0){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f2 <- function(x, y, p = 0.4){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f3 <- function(x, y, p = 0.8){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+opar <- par(no.readonly = TRUE) # copy of current settings
+par(mfrow = c(1, 3), mar = c(1.1, 1.1, 1.1, 1.1), pty = "s")
+x <- seq(-3, 3, length = 40)
+y <- x
+persp(x, y, outer(x, y, f1), zlab = "z", main = expression(rho == 0),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.2)
+persp(x, y, outer(x, y, f2), zlab = "z", main = expression(rho == 0.4),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.2)
+persp(x, y, outer(x, y, f3), zlab = "z", main = expression(rho == 0.8),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.3)
+par(opar)
+
+
+f1 <- function(x, y, p = 0){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f2 <- function(x, y, p = -0.4){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f3 <- function(x, y, p = -0.8){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+opar <- par(no.readonly = TRUE) # copy of current settings
+par(mfrow = c(1, 3), mar = c(1.1, 1.1, 1.1, 1.1), pty = "s")
+x <- seq(-3, 3, length = 40)
+y <- x
+persp(x, y, outer(x, y, f1), zlab = "z", main = expression(rho == 0),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.2)
+persp(x, y, outer(x, y, f2), zlab = "z", main = expression(rho == -0.4),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.2)
+persp(x, y, outer(x, y, f3), zlab = "z", main = expression(rho == -0.8),
+      theta = -25, expand = 0.65, phi = 25, shade = 0.2)
+par(opar)
+
+
+f1 <- function(x, y, p = 0){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f2 <- function(x, y, p = 0.4){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+f3 <- function(x, y, p = 0.8){
+  exp( (x^2 - 2*p*x*y + y^2) / (-2*(1 - p^2)) ) / (2*pi*sqrt(1 - p^2)) }
+opar <- par(no.readonly = TRUE) # copy of current settings
+par(mfrow = c(1, 3), mar = c(4.1, 4.1, 4.1, 1.1), pty = "s")
+x <- seq(-3, 3, length = 50)
+y <- x
+contour(x, y, outer(x, y, f1), nlevels = 10, xlab = "x", ylab = "y",
+        main = expression(rho == 0))
+contour(x, y, outer(x, y, f2), nlevels = 10, xlab = "x", ylab = "y",
+        main = expression(rho == 0.4))
+contour(x, y, outer(x, y, f3), nlevels = 10, xlab = "x", ylab = "y",
+        main = expression(rho == 0.8))
+par(opar)
+
+
+opar <- par(no.readonly = TRUE) # copy of current settings
+par(mfrow = c(1, 3), mar = c(4.1, 4.1, 4.1, 1.1), pty = "s")
+x <- seq(-3, 3, length = 50)
+y <- x
+image(x, y, outer(x, y, f1), col  = gray((0:100)/100), xlab = "x",
+      ylab = "y", main = expression(rho == 0))
+image(x, y, outer(x, y, f2), col  = gray((0:100)/100), xlab = "x",
+      ylab = "y", main = expression(rho == 0.4))
+image(x, y, outer(x, y, f3), col  = gray((0:100)/100), xlab = "x",
+      ylab = "y", main = expression(rho == 0.8))
+
+
+x <- seq(-3, 3, length = 50)
+y <- x
+z1 <- outer(x, y, f1)
+z2 <- outer(x, y, f2)
+z3 <- outer(x, y, f3)
+Grid <- expand.grid(x = x, y = y)
+zp <- c(expression(rho == 0.0), expression(rho == 0.4),
+        expression(rho == 0.8))
+DF1 <- data.frame(x = Grid$x, y = Grid$y, z = as.vector(z1))
+DF2 <- data.frame(x = Grid$x, y = Grid$y, z = as.vector(z2))
+DF3 <- data.frame(x = Grid$x, y = Grid$y, z = as.vector(z3))
+DF1$r = "rho == 0.0"
+DF2$r = "rho == 0.4"
+DF3$r = "rho == 0.8"
+BDF <- rbind(DF1, DF2, DF3)
+p <- ggplot(data = BDF, aes(x = x, y = y, z = z))
+p + stat_contour(aes(colour = ..level..)) + theme_bw() +
+  scale_colour_gradient(low = "gray10", high = "gray90") +
+  labs(colour = "Density", x = "x", y = "y") +
+  facet_grid(. ~ r, labeller = label_parsed) +
+  coord_fixed(ratio = 1)
+
+p <- ggplot(data = BDF, aes(x = x, y = y, fill = z))
+p + geom_raster() + theme_bw() +
+  scale_fill_gradient(low = "gray10", high = "gray90") +
+  labs(fill = "Density", x = "x", y = "y") + facet_grid(. ~ r) +
+  coord_fixed(ratio = 1)
+
+contourplot(z1 + z2 + z3 ~ x * y, data = Grid, xlab = "x", ylab = "y",
+            outer = TRUE, layout = c(3, 1), aspect = "xy",
+            cuts = 11, strip = strip.custom(factor.levels = zp))
+
+
+levelplot(z1 + z2 + z3 ~ x * y, data = Grid, xlab = TeX("$x$"), ylab = TeX("$y$"), 
+          outer = TRUE, layout = c(3, 1), aspect = "xy",
+          strip = strip.custom(factor.levels = zp))
+
+
+# Sampling: What We Really Observe.
+set.seed(1066)
+mu <- matrix(c(0,0), nrow = 2, ncol = 1, byrow = TRUE)
+rho <- 0.0
+Sigma <- matrix(c(1, rho, rho, 1), nrow=2, ncol=2, byrow = TRUE) 
+data = data.frame(mvrnorm(n = 1000, mu, Sigma))
+plot(data$X2, data$X1, xlab=TeX("$X_1$"), ylab=TeX("$X_2$"), 
+     main="Scatterplot of Simulated Data", pch=16, col='blue',
+     xlim=c(-4,4), ylim=c(-4,4))
+grid(lw = 2)
+
+# What Do We Do?
+abline(lm(X2 ~ X1, data = data), col='red')
+
+
+set.seed(1066)
+mu <- matrix(c(0,0), nrow = 2, ncol = 1, byrow = TRUE)
+rho <- 0.5
+Sigma <- matrix(c(1, rho, rho, 1), nrow=2, ncol=2, byrow = TRUE) 
+data = data.frame(mvrnorm(n = 1000, mu, Sigma))
+plot(data$X2, data$X1, xlab=TeX("$X_1$"), ylab=TeX("$X_2$"), 
+     main="Scatterplot of Simulated Data", pch=16, col='blue',
+     xlim=c(-4,4), ylim=c(-4,4))
+grid(lw = 2)
+abline(lm(X2 ~ X1, data = data), col='red')
+
+
+set.seed(1066)
+rho <- -0.5
+Sigma <- matrix(c(1, rho, rho, 1), nrow=2, ncol=2, byrow = TRUE) 
+data = data.frame(mvrnorm(n = 1000, mu, Sigma))
+plot(data$X2, data$X1, xlab=TeX("$X_1$"), ylab=TeX("$X_2$"), 
+     main="Scatterplot of Simulated Data", pch=16, col='blue',
+     xlim=c(-4,4), ylim=c(-4,4))
+grid(lw = 2)
+abline(lm(X2 ~ X1, data = data), col='red')
+
+
+# Real Data: A Synthesis of the Above.
+data("mtcars")
+attach(mtcars)
+
+sapply(mtcars, mean, na.rm=TRUE)
+sapply(mtcars, var, na.rm=TRUE)
+cor(mtcars)
+
+plot(mpg, wt, xlab=TeX("$MPG$"), ylab=TeX("Weight$"), 
+     main="Weight versus MPG", pch=16, col='blue')
+grid(lw = 2)
+
+abline(lm(wt ~ mpg, data = mtcars), col='red')
+
+
+threemonth <- drop_na(fredr(series_id = "DGS3MO", observation_start = as.Date("2008-01-01")))
+tenyear <- drop_na(fredr(series_id = "DGS10", observation_start = as.Date("2008-01-01")))
+plot(threemonth$value, tenyear$value, 
+     xlab=TeX("3 Month Yields"), ylab=TeX("10 Year Yields"), 
+     main="Daily Interest Rates Since 2008", pch=16, col='blue')
+grid(lw = 2)
+abline(lm(tenyear$value ~ threemonth$value), col='red')
